a3c146c6ae29569c1d912300e4c71eb79ac6e03f
Martin Cosmen Cuesta <martin.cosmencuesta@plexus.tech>
2026-09-04 08:36:45 +0200

    Variables y tipos

---
+++ python/leccion-01/01_variables_y_tipos.py
@@ -0,0 +1,24 @@
+# TEXTO (str)
+nombre = "Martín"
+empresa = "Plexus"
+
+# NÚMERO ENTERO (int)
+edad = 25
+
+# NÚMERO DECIMAL (float)
+altura = 1.75
+
+# VERDADERO O FALSO (bool)
+le_gusta_el_futbol = True
+
+# Mostrar valores
+print(nombre)
+print(edad)
+print(altura)
+print(le_gusta_el_futbol)
+
+# Mostrar tipos de datos
+print(type(nombre))
+print(type(edad))
+print(type(altura))
+print(type(le_gusta_el_futbol))
--- python/leccion-01/primer_programa.py
+++
@@ -1,9 +0,0 @@
-media = int(input("Introduce la valoración del jugador: "))
-if media >= 90:
-    print("Increíble, eres un jugador top mundial")
-elif media >= 70:
-    print("Eres un buen jugador")
-elif media >= 50:
-     print("Estás en la media de los jugadores a nivel mundial")
-else:
-    print("Eres un matao")
