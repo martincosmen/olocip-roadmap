aa43279f26641220f11bbbe4dc35e1fc1451b0a3
Martin Cosmen Cuesta <martin.cosmencuesta@plexus.tech>
2026-09-04 08:39:50 +0200

    Input

---
+++ python/leccion-01/04_input.py
@@ -0,0 +1,12 @@
+nombre = input("¿Cómo te llamas? ")
+
+edad = int(input("¿Cuántos años tienes? "))
+
+edad_dentro_10_anos = edad + 10
+
+print(
+    "Hola,", nombre,
+    "dentro de 10 años tendrás",
+    edad_dentro_10_anos,
+    "años"
+)
