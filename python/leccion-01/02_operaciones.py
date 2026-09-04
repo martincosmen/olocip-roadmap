37c77ed52f8577661dc8a9aa806f7505e9e9a256
Martin Cosmen Cuesta <martin.cosmencuesta@plexus.tech>
2026-09-04 08:37:50 +0200

    Operaciones

---
+++ python/leccion-01/02_operaciones.py
@@ -0,0 +1,18 @@
+edad = 25
+
+# Suma
+print(edad + 5)
+
+# Resta
+print(edad - 5)
+
+# Multiplicación
+print(edad * 2)
+
+# División
+print(edad / 2)
+
+# Guardar un resultado en una nueva variable
+edad_dentro_10_anos = edad + 10
+
+print(edad_dentro_10_anos)
