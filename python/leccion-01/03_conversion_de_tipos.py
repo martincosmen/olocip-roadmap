8c69a3b5621982961f1f0c5dcda390cc3612222c
Martin Cosmen Cuesta <martin.cosmencuesta@plexus.tech>
2026-09-04 08:38:39 +0200

    Conversion de tipos

---
+++ python/leccion-01/03_conversion_de_tipos.py
@@ -0,0 +1,34 @@
+# Convertir texto a entero
+numero_texto = "25"
+
+numero_entero = int(numero_texto)
+
+print(numero_entero)
+print(type(numero_entero))
+
+
+# Convertir texto decimal a float
+numero_decimal_texto = "25.5"
+
+numero_decimal = float(numero_decimal_texto)
+
+print(numero_decimal)
+print(type(numero_decimal))
+
+
+# Convertir float a entero
+numero_float = 25.9
+
+numero_int = int(numero_float)
+
+print(numero_int)
+print(type(numero_int))
+
+
+# Convertir número a texto
+numero = 25
+
+numero_texto = str(numero)
+
+print(numero_texto)
+print(type(numero_texto))
