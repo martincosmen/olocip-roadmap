af22a52ac64514fa39ce694c95058394af87a965
Martín Cosmen <martincosmen10@gmail.com>
2026-09-07 22:01:25 +0200

    pruebas

--- python/leccion-01/08_bucles_for.py
+++ python/leccion-01/08_bucles_for.py
@@ -1,4 +1,14 @@
+# Crear una lista
 jugadores = ["Lamine", "Pedri", "Gavi", "Cubarsí"]
 
+# Recorrer todos los elementos
 for jugador in jugadores:
     print(jugador)
+
+
+
+    numeros = [5, 12, 8, 25, 3, 18]
+
+for numero in numeros:
+    if numero >= 10:
+        print(numero, "es mayor o igual que 10")
